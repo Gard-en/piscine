e14848815a6448a0b144875180141c3b458493bd
Sam Catcheside <scatches@F2N001.42adel.org.au>
2023-01-25 16:41:37 +1030

    aa

---
+++ C00/ex07/ft_putnbr.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: scatches <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/25 15:45:24 by scatches          #+#    #+#             */
+/*   Updated: 2023/01/25 15:45:26 by scatches         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+#include <stdio.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int n)
+{
+	if (n < 0)
+	{
+		ft_putchar('-');
+		n = -n;
+	}
+	if (n < 9)
+	{
+		ft_putchar(n + '0');
+	}
+	else
+	{
+		ft_putnbr(n / 10);
+		ft_putnbr(n % 10);
+	}
+}
+
+int	main(void)
+{
+	ft_putnbr(42);
+	return (0);
+}
