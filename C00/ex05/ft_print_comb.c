d1201830b3c732f11a8b9f6a66a56f902da6861f
Sam Catcheside <scatches@F2N001.42adel.org.au>
2023-01-25 14:30:47 +1030

    a

--- C00/ex05/ft_print_comb.c
+++ C00/ex05/ft_print_comb.c
@@ -9,5 +9,37 @@
 /*   Updated: 2023/01/25 12:15:27 by scatches         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <stdio.h>
+#include <unistd.h>
 
+void	ft_print_comb(void)
+{
+	int		i;
+	int		j;
+	int		k;
+	int		l;
+	char	output[4];
 
+	i = 0;
+	while (i < 1000)
+	{
+		j = i / 100;
+		k = (i / 10) % 10;
+		l = i % 10;
+		if (j < k && k < l)
+		{
+			output[0] = j + '0';
+			output[1] = k + '0';
+			output[2] = l + '0';
+			output[3] = ' ';
+			write(1, &output[0], sizeof(output));
+		}
+		i++;
+	}
+}
+
+int	main(void)
+{
+	ft_print_comb();
+	return (0);
+}
