fa1b7c97920166e0463d92b8386c1e223d5dccca
Sam Catcheside <scatches@F2N010.42adel.org.au>
2023-01-26 15:18:47 +1030

    ab

--- C00/ex08/ft_print_combn.c
+++ C00/ex08/ft_print_combn.c
@@ -17,33 +17,29 @@
 	Start from the right and work your way left
 */
 
-void	ft_print_combn(int n)
+void    ft_print_combn(int n)
 {
-	char	output[11];
-	int		i;
+    char    t[11];
+    int        i;
 
-	if (n < 1 || n > 9)
-	return;
-
-	*t = '0';
-	i = 0;
-
-	while (i++ < n)
-	{
-		output[i] = t[i -1] + 1;
-		t[n] = ',';
-		t[n + 1 ] = ' ';
-		while (*t <= 58 - n)
-		{
-			write(1, t, n + ((*t != 58 -n) ? 2 : 0));
-			i = n;
-			while(i--)
-				if (++t[i] <= 58 - n + i)
-					break;
-			while (i++ > 0 && i < n)
-				t[i] = t[i -1] + 1;
-		}
-	} 
+    if (n < 1 || n > 9)
+        return ;
+    *t = '0';
+    i = 0;
+    while (++i < n)
+        t[i] = t[i - 1] + 1;
+    t[n] = ',';
+    t[n + 1] = ' ';
+    while (*t <= 58 - n)
+    {
+        write(1, t, n + ((*t != 58 - n) ? 2 : 0));
+        i = n;
+        while (i--)
+            if (++t[i] <= 58 - n + i)
+                break ;
+        while (++i > 0 && i < n)
+            t[i] = t[i - 1] + 1;
+    }
 }
 
 int main(void)
