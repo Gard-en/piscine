f6ff26b2f719eb3ea871d2374e24a4f257c6f644
Gard-en <gard.en@outlook.com>
2023-01-29 09:06:24 +1030

    rush00

---
+++ Rush00/ex00/rush00.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: scatches <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/25 12:15:25 by scatches          #+#    #+#             */
+/*   Updated: 2023/01/25 12:15:27 by scatches         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+char	ft_getchar(const int row, const int col, const int x, const int y)
+{
+	char	outchar;
+
+	outchar = 'o';
+	if (row == 0 || row == x - 1)
+	{
+		outchar = '-';
+		if (col == 0 || col == y - 1)
+			outchar = 'o';
+	}
+	else
+	{
+		outchar = ' ';
+		if (col == 0 || col == y - 1)
+			outchar = '|';
+	}
+	return (outchar);
+}
+
+void	rush(const int x, const int y)
+{
+	int		row;
+	int		col;
+	char	outchar;
+
+	row = -1;
+	while (++row != x)
+	{
+		col = -1;
+		while (++col != y)
+		{
+			outchar = ft_getchar(row, col, x, y);
+			ft_putchar(outchar);
+		}
+		ft_putchar('\n');
+	}
+}
