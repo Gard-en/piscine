45c34ba6ee489ab8caf12b71d14e0554297387b6
Sam Catcheside <scatches@F2N016.42adel.org.au>
2023-01-31 18:38:57 +1030

    aa

---
+++ C01/ex03/ft_div_mod.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_div_mod.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: scatches <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/31 16:46:15 by scatches          #+#    #+#             */
+/*   Updated: 2023/01/31 16:46:18 by scatches         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_div_mod(int a, int b, int *div, int *mod)
+{
+	*div = a / b;
+	*mod = a % b;
+	ft_putchar(*div + 48);
+	ft_putchar(' ');
+	ft_putchar(*mod + 48);	
+}
+
+int	main(void)
+{
+	int div;
+	int mod;
+
+	ft_div_mod(10,7, &div, &mod);
+	return (0);
+}
---
+++ C01/ex07/ft_rev_int_tab.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_int_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: scatches <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/31 17:03:46 by scatches          #+#    #+#             */
+/*   Updated: 2023/01/31 17:03:47 by scatches         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void    ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+void ft_rev_int_tab(int *tab, int size)
+{
+    int i;
+
+    i = size;
+    while(i-- >= 0)
+    {
+        ft_putchar(*(tab + i) + 48);
+    }
+}
+
+int main(void)
+{
+    int array[3] = {1,2,3};
+    int *tab = array;
+
+    ft_rev_int_tab(tab, sizeof 3);
+    return (0);
+}
+
