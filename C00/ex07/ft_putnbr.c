1aeaaa0c9aff7ca3bd1733ff5da267a03d4ad67a
Sam Catcheside <scatches@F2N016.42adel.org.au>
2023-01-31 14:21:20 +1030

    safeguard against seg fault from overflow

--- C00/ex07/ft_putnbr.c
+++ C00/ex07/ft_putnbr.c
@@ -18,25 +18,31 @@ void	ft_putchar(char c)
 
 void	ft_putnbr(int n)
 {
+	if (n == -2147483648)
+	{
+		ft_putchar('-');
+		ft_putchar('2');
+		n = 147483648;
+	}
 	if (n < 0)
 	{
 		ft_putchar('-');
 		n = -n;
 	}
-	if (n < 9)
-	{
-		ft_putchar(n + '0');
-	}
-	else
+	if (n >= 10)
 	{
 		ft_putnbr(n / 10);
 		ft_putnbr(n % 10);
 	}
+	else
+	{
+		ft_putchar(n + '0');
+	}
 }
-/*
+
 int	main(void)
 {
-	ft_putnbr(42);
+	ft_putnbr(4766);
 	return (0);
 }
-*/
+
