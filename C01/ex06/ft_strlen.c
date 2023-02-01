2814dae8df3aa8103e1377cfdd1f4a2f510838eb
Gard-en <gard.en@outlook.com>
2023-02-01 16:04:35 +1030

    ft_strlen

--- C01/ex06/ft_strlen.c
+++ C01/ex06/ft_strlen.c
@@ -1,21 +1,43 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: scatches <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/01 14:16:06 by scatches          #+#    #+#             */
-/*   Updated: 2023/02/01 14:17:59 by scatches         ###   ########.fr       */
+/*   Created: 2023/01/31 18:08:39 by scatches          #+#    #+#             */
+/*   Updated: 2023/01/31 18:08:41 by scatches         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <unistd.h>
 
-void    ft_strcpy(char *dest, char *src)
+void	ft_putstr(char *str)
 {
-    
+	while (*str != '\0')
+	{
+		write(1, str, 1);
+		str++;
+	}
 }
 
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (*str != '\0')
+	{
+		str++;
+		i++;
+	}
+	return (i);
+}
+/*
 int main(void)
 {
-    return (0);
+	char str[] = "hello";
+	char *ptr = str;
+	ft_strlen(ptr);
+	return (0);
 }
+*/
